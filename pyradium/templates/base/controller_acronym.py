285133048e54018a48bd7071104ecab5448d661c
Johannes Bauer <joe@johannes-bauer.com>
2021-11-04 21:30:06 +0100

    Emit less long acronyms per slide
    We have some poor heuristic that estimates the number of acronyms that
    fit on one slide.

--- pyradium/templates/base/controller_acronym.py
+++ pyradium/templates/base/controller_acronym.py
@@ -26,13 +26,31 @@ class AcronymController(BaseController):
 	def acronyms_per_slide(self):
 		return self._options["acronyms_per_slide"]
 
+	@property
+	def chars_per_line(self):
+		return self._options.get("chars_per_line", 45)
+
 	def render(self):
 		acronyms = self.rendered_presentation.renderer.get_custom_renderer("acronym")
 		used_acronyms = list(acronyms.get_all_used_acronyms())
 		pages = (len(used_acronyms) + self.acronyms_per_slide - 1) // self.acronyms_per_slide
 		additional_slide_var_list = [ ]
-		for page in range(pages):
-			page_content = used_acronyms[page * self.acronyms_per_slide : (page + 1) * self.acronyms_per_slide]
-			additional_slide_vars = { "acronyms": page_content }
-			additional_slide_var_list.append(additional_slide_vars)
+
+		current_page = [ ]
+		used_lines = 0
+		for acronym in used_acronyms:
+			acronym_len = len(acronym.acronym) + 2 + len(acronym.text)
+			lines = (acronym_len + self.chars_per_line - 1) // self.chars_per_line
+			if (used_lines > 0) and (used_lines + lines > self.acronyms_per_slide):
+				# Emit a new page
+				used_lines = 0
+				additional_slide_var_list.append({ "acronyms": current_page })
+				current_page = [ ]
+			used_lines += lines
+			current_page.append(acronym)
+
+		if len(current_page) > 0:
+			# Emit the last page
+			additional_slide_var_list.append({ "acronyms": current_page })
+
 		yield from self.slide.emit_nocontent_slide(self.rendered_presentation, self.content_containers, additional_slide_var_list)
