44b884599441e6983e0b6fa39c29fe817d7e8d32
Frederik Reiter <kontakt@frederikreiter.de>
2022-06-01 18:26:37 +0200

    Add prompt attribute to <s:term> tag
    If a prompt attribute is present, make all commands that start with this
    prompt appear inside a <span> which is one-click selectable. This patch
    fixes #12 (issue) and closes #35 (PR).

--- pyradium/xmlhooks/TerminalHook.py
+++ pyradium/xmlhooks/TerminalHook.py
@@ -29,7 +29,37 @@ class TerminalHook(InnerTextHook):
 	def handle_text(cls, text, rendered_presentation, node):
 		replacement_node = node.ownerDocument.createElement("pre")
 		replacement_node.setAttribute("class", "terminal")
-		replacement_node.appendChild(node.ownerDocument.createTextNode(text))
+		if node.hasAttribute("prompt"):
+			prompt = node.getAttribute("prompt")
+			lines = text.splitlines(keepends = True)
+			while len(lines) > 0:
+				line = lines.pop(0)
+				if not line.startswith(prompt):
+					replacement_node.appendChild(node.ownerDocument.createTextNode(line))
+					continue
+				command = line[len(prompt) : ]
+				replacement_node.appendChild(node.ownerDocument.createTextNode(prompt))
+				while line.strip().endswith("\\"):
+					line = lines.pop(0)
+					command += line
+
+				# If there is a trailing newline, we do not want this inside
+				# the <span> or otherwise the pasted output on the terminal
+				# will contain it too
+				has_trailing_nl = command.endswith("\n")
+				command = command.rstrip("\n")
+				command_node = node.ownerDocument.createElement("span")
+				command_node.setAttribute("class", "command")
+				command_node.appendChild(node.ownerDocument.createTextNode(command))
+				replacement_node.appendChild(command_node)
+
+				if has_trailing_nl:
+					# In this case we need to insert the newline back into the
+					# "normal" (non-copied) text.
+					replacement_node.appendChild(node.ownerDocument.createTextNode("\n"))
+		else:
+			replacement_node.appendChild(node.ownerDocument.createTextNode(text))
+
 		if node.hasAttribute("height"):
 			replacement_node.setAttribute("style", "height: %s" % (node.getAttribute("height")))
 		return replacement_node
